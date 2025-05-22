e308825e7181188cf3cca0c0cb1018bc4f240f5a
Adarsh namdeo <adarshnamdeo304@gmail.com>
2025-05-22 15:49:57 +0000

    container config done

--- basics/arrays.js
+++ basics/arrays.js
@@ -28,13 +28,37 @@ const myArr2 = new Array (1,2,3,4,5)
 
 // slice or splice
 
-console.log("A", myArr)
-const myn1 = myArr.slice(1,3)
+// console.log("A", myArr)
+// const myn1 = myArr.slice(1,3)
 
-console.log( myn1);  // [1,2]
-console.log("B", myArr);
+// console.log( myn1);  // [1,2]
+// console.log("B", myArr);
 
-const myn2 = myArr.splice(1,3)
-console.log("c", myArr); //[0,4,5,6]
-console.log(myn2);
+// const myn2 = myArr.splice(1,3)
+// console.log("c", myArr); //[0,4,5,6]
+// console.log(myn2);
 
+
+// Marvel_hero= [ "thor", " Ironman", " Captain America"]
+// Dc_hero= ["flash", "batman", "superman"]
+
+// all_Heros= Marvel_hero.concat(Dc_hero)// to merge the  both
+// console.log(all_Heros)
+
+// const all_new_heros= [...Marvel_hero,...Dc_hero]
+// console.log(all_new_heros);
+
+
+const another_array= [1,2,4,[3,6,5],23,54,23,[34,23,1,1,[12,23,45,65,]]]
+console.log(another_array.flat( Infinity)) //flat usage
+
+console.log(Array.isArray("adarsh")) //checking its array or not (answer in true or false)
+console.log(Array.from("adarsh")) // converting into array
+console.log(Array.from({name:"adarsh"})) // interesting point //  "[]"
+
+let score1 =100;
+let score2=200;
+let score3 =300;
+let score4 =400;
+
+console.log(Array.of(score1,score2,score3,score4))
