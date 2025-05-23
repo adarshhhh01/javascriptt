5db52d648418a6c971ed7942e499c01ae4a69628
Adarsh namdeo <adarshnamdeo304@gmail.com>
2025-05-23 15:14:44 +0000

    container config done

--- basics/objects.js
+++ basics/objects.js
@@ -38,14 +38,38 @@ const obj2 = {3:"c", 4:"d"}
 
 // const obj3= {obj1, obj2}; //{ obj1: { '1': 'a', '2': 'B' }, obj2: { '3': 'c', '4': 'd' } }
 // const obj3 = Object.assign(obj1, obj2)  //{ '1': 'a', '2': 'B', '3': 'c', '4': 'd' }
-const obj3 = Object.assign({},obj1, obj2)  // know to be good practice{ '1': 'a', '2': 'B', '3': 'c', '4': 'd' }
-const obj4= {...obj1,...obj2}
-console.log(obj3)
-console.log(obj4)   //{ '1': 'a', '2': 'B', '3': 'c', '4': 'd' }
+// const obj3 = Object.assign({},obj1, obj2)  // know to be good practice{ '1': 'a', '2': 'B', '3': 'c', '4': 'd' }
+// const obj4= {...obj1,...obj2}
+// console.log(obj3)
+// console.log(obj4)   //{ '1': 'a', '2': 'B', '3': 'c', '4': 'd' }
 
 
-user[1].email
-console.log(tinderUser);
+// user[1].email
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+
+
+// *******************objects destructureing *****************
+
+const course = {
+    courseName: " js in hindi",
+    price: "999",
+    courseInstructor: "adarsh"
+
+}
+
+
+///course.constructor
+
+const {courseInstructor:instructor}= course
+console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name" : "adarsh"
+//     "courseName": "Js in hindi"
+//     "price":"free"
+// }
